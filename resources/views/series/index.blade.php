4a688f50637bfd7b91a7aee02eabaf6668ae3205
Gabriel447 <gabriel_447@live.com>
2024-09-20 00:31:52 -0300

    adicionando botao

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Séries">
-    <a class="mx-1" href="/series/criar">Adicionar</a>
+    <a class="btn btn-dark" href="/series/criar">Adicionar</a>
     <div class="my-2 w-50">
         <ul class="list-group">
             @foreach ($series as $serie)
