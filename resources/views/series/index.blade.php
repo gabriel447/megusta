e89947cc1f276304ae8c07b51db3ab3aca5fdb0e
Gabriel <gabriel.senra@novagne.com.br>
2024-09-23 17:33:51 -0300

    listando os episodios e temporadas

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,15 +1,19 @@
 <x-layout title="Séries">
     <a href="{{ route('series.create') }}" class="btn btn-dark">Adicionar</a>
+
     @isset($mensagemSucesso)
     <div class="alert alert-success mt-3">
         {{ $mensagemSucesso }}
     </div>
     @endisset
+
     <div class="my-2 w-50">
         <ul class="list-group">
             @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                {{ $serie->nome }}
+                <a href="{{ route('seasons.index', $serie->id) }}">
+                    {{ $serie->nome }}
+                </a>
 
                 <div class="d-flex">
                     <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm mx-2">E</a>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 
 Route::get('/', function () {
-    return redirect('series');
+    return redirect('/series');
 });
 
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)->except(['show']);
+
+Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
