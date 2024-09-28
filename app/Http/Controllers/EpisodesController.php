d8a8b6001fa8f748ab70ec09fe1b200e67740eb9
Gabriel447 <gabriel_447@live.com>
2024-09-28 20:54:34 -0300

    implementado numero de episodios marcados

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -10,7 +10,10 @@ class EpisodesController
 {
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', [
+            'episodes' => $season->episodes,
+            'mensagemSucesso' => session('mensagem.sucesso')
+        ]);
     }
 
     public function update(Request $request, Season $season)
@@ -21,6 +24,6 @@ class EpisodesController
         });
         $season->push();
 
-        return to_route('episodes.index', $season->id);
+        return to_route('episodes.index', $season->id)->with('mensagem.sucesso', 'Episódios marcados com sucesso!');    
     }
 }
