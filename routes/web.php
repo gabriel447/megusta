83a5d581683596cc82c062f38dfba3658344020c
Gabriel447 <gabriel_447@live.com>
2024-09-29 16:41:38 -0300

    Route names

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ Route::resource('/series', SeriesController::class)->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index'); 
 
-route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 
-route::post('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
 
-route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
 
-route::get('/login', [LoginController::class, 'index'])->name('login');
-route::post('/login', [LoginController::class, 'store'])->name('signin');
-route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('signin');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
 Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-route::post('/register', [UsersController::class, 'store'])->name('users.store');
+Route::post('/register', [UsersController::class, 'store'])->name('users.store');
