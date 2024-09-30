12d4eb5752210e8031d4834017b6890a1aeceb60
Gabriel447 <gabriel_447@live.com>
2024-09-30 14:49:06 -0300

    auth

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,23 +1,30 @@
 <x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
-    <a href="{{ route('series.create') }}" class="btn btn-dark">Adicionar</a>
-    
+    @auth
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
+    @endauth    
     <div class="mt-2 mb-2">
         <ul class="list-group">
             @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <a href="{{ route('seasons.index', $serie->id) }}">
-                    {{ $serie->nome }}
-                </a>
+            @auth <a href="{{ route('seasons.index', $serie->id) }}"> @endauth
+            {{ $serie->nome }}
+            @auth </a> @endauth
 
-                <div class="d-flex">
-                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm mx-2">E</a>
+                @auth
+                <span class="d-flex">
+                        <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                                    E
+                                </a>
 
-                    <form action="{{ route('series.destroy', $serie->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button class="btn btn-danger btn-sm">X</button>
-                    </form>
-                </div>
+                        <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger btn-sm">
+                                    X
+                                </button>
+                        </form>
+                </span>
+                @endauth
 
             </li>
             @endforeach
