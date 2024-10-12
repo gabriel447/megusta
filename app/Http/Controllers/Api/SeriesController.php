5c4e608a4ee34882ad4bc1862dccff889d35d445
Gabriel447 <gabriel_447@live.com>
2024-10-12 20:47:04 -0300

    crud completo

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Repositories\SeriesRepository;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
@@ -9,12 +10,35 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index() {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
+    public function index()
+    {
         return Series::all();
     }
 
     public function store(SeriesFormRequest $request)
     {
-        return response()->json(Series::create($request ->all()), 201);
+        return response()->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(Series $series)
+    {
+        return $series;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+        return $series;
+    }
+    
+    public function destroy(int $series) 
+    {
+       Series::destroy($series);
+       return response()->noContent();
     }
 }
